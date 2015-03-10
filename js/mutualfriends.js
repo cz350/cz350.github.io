b41f33f70a45c4c32dac62aacc98f613513cb3b4
cz350 <khnahu@gmail.com>
2015-03-10 21:06:02 +0200

    Првавка js

--- js/mutualfriends.js
+++ js/mutualfriends.js
@@ -51,7 +51,7 @@ function invite() {
 	VK.callMethod('showInviteBox');
 }
 function wall() {
-	VK.api('wall.post', {message: 'Просмотр общих друзей и подписчиков у любых людей и сообществ! https://vk.com/app4236781', attachments: 'photo33610634_350256389, https://vk.com/app4236781'}, function(data) {
+	VK.api('wall.post', {message: 'Просмотр общих друзей и подписчиков у любых людей и сообществ! https://vk.com/app4820427', attachments: 'photo33610634_350256389, https://vk.com/app4820427'}, function(data) {
 
 	});
 }
